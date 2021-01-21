f610f381690dd3a8daec59e87e928a2a6f097287
Suraj Thakur <surajkumar2091@gmail.com>
2021-01-21 05:42:04 +0530

    Empty CD Wars

--- cdwars.cpp
+++ cdwars.cpp
@@ -2,48 +2,7 @@
 using namespace std;
 
 
-bool isValidWalk(std::vector<char> walk) {
-	if (walk.size() > 10)
-		return false;
-	if (walk.size() < 4)
-		return false;
-	int n = 0, s = 0, e = 0, w = 0;
-	for (int i = 0; i < 10; i++) {
-		if (walk[i] == 'n' && n >= 0 && s == 0) {
-			n++;
-		}
-		else if (walk[i] == 's' && s >= 0 && n == 0) {
-			s++;
-		}
-		else if (walk[i] == 'n' && s > 0) {
-			s--;
-		}
-		else if (walk[i] == 's'&n > 0) {
-			n--;
-		}
-
-		else if (walk[i] == 'e' && e >= 0 && w == 0) {
-			e++;
-		}
-		else if (walk[i] == 'w' && w >= 0 && e == 0) {
-			w++;
-		}
-		else if (walk[i] == 'e' && w > 0) {
-			w--;
-		}
-		else if (walk[i] == 'w' && e > 0) {
-			e--;
-		}
-
-	}
-	if (n == 0 && w == 0 && e == 0 && s == 0)
-		return true;
-	return false;
-
-}
-
 int main() {
-
 
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);

f8254454772b0140a7faddb2e106f4ce871aad10
Suraj Thakur <surajkumar2091@gmail.com>
2021-01-25 14:31:08 +0530

    LeetCode String Breaker

---
+++ collisionOfStrings.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	string s1 = "jgwoajzztsdtlyoitbbzkfzeuddnushxgqqmdwgmvqewsixawdzgysmvprthibufvvrqhniyvnm";
+	string s2 = "ipdvefraoybpgmxrkhdcvxbnogftqgqmqlghlvsyyckbobtfejpbsqcsmcmzqsujmilpbrpanjs";
+
+
+	sort(s1.begin(), s1.end()) ;
+	sort(s2.begin(), s2.end()) ;
+
+	cout << s1 << endl << s2 << endl;
+}
+
+
+
+

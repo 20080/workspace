0d82d33bf578843f4b5fe4c369d27e15c6dc73dd
Suraj Thakur <surajkumar2091@gmail.com>
2021-01-25 14:34:26 +0530

    full code string breaker

--- collisionOfStrings.cpp
+++ collisionOfStrings.cpp
@@ -17,4 +17,42 @@ int main() {
 
 
 
+bool checkIfCanBreak(string s1, string s2) {
+
+	if (s1.length() != s2.length())
+		return false;
+	sort(s1.begin(), s1.end());
+	sort(s2.begin(), s2.end());
+
+	if (s1 == s2)
+		return true;
+	int zz = s1.length() - 1;
+	while (s1[zz] == s2[zz])
+		zz--;
+	bool desive = s1[zz] > s2[zz]; //s1 is greater overall
+
+	if (desive) {
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1[i] >= s2[i])
+				continue;
+			else {
+				return false;
+			}
+		}
+
+	}
+	else {
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1[i] <= s2[i])
+				continue;
+			else {
+				return false;
+			}
+		}
+	}
+
+	return true;
+
+
+}
 

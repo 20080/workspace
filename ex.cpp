8b6a6d4177594f601d9ce72afffb9f7d15c495f7
Suraj Thakur <surajkumar2091@gmail.com>
2021-01-30 11:23:37 +0530

    leetCDE hard

--- ex.cpp
+++ ex.cpp
@@ -1,45 +1,33 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using  namespace std;
 
-int sup (int n) {
 
-	long long ans = 1, bits = 0, num;
-	for (int i = 2; i <= n; i++)
-	{
-		bits = 0;
-		num = i;
-		while (num != 0)
-		{
-			num >>= 1;
-			bits++;
-		}
-
-		ans = ans << bits;
-		ans = (ans | i) ;
-		ans = ans % 1000000007;
-	}
-	return ans;
-
-
-	// if (n == 1)
-	// 	return 1;
-	// long ns = sup(n - 1);
-
-	// int bitsNum = 0;
-	// int num = n;
-	// while (num) {
-	// 	num >>= 1;
-	// 	bitsNum++;
-	// }
-
-	// ns <<= bitsNum;
-	// ns = ns | n;
-	// ns %= (int)1e9 + 7;
-	// return ns;
-}
 
 
 int main() {
 
-	cout << sup(42) << endl;
+
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int m, n;
+	cin >> m >> n;
+	vector<vector<int>> v(m, vector<int> (n, 0));
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cin >> v[i][j];
+		}
+	}
+
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cout << v[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+
 }
--- leetCodeRandom.cpp
+++ leetCodeRandom.cpp
@@ -3,13 +3,33 @@ using namespace std;
 #define ll long long
 #define vec vector<int>
 
-int swap
-
-void solve() {
-
-	int n = 10;
 
 
+int solve(vector<vector<int>> grid, int k) {
+
+	int m = grid.size(), n = grid[0].size();
+	queue<vector<int>>q({{0, 0, k, 0}});
+	int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, { -1, 0}};
+
+	while (!q.empty()) {
+		auto val = q.front();
+		q.pop();
+		int x = val[0], y = val[1], curK = val[2], step = val[3];
+		if (x == m - 1 && y == n - 1)
+			return step;
+		for (auto d : dir) {
+			int nx = x + d[0], ny = y + d[1];
+			if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+				int nk = curK - grid[nx][ny];
+				if (nk > -1) {
+					q.push({nk, ny, nk, step + 1});
+				}
+
+			}
+		}
+
+	}
+	return -1;
 
 
 }
@@ -21,8 +41,16 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
-	// int test;
-	// cin >> test;
-	// while (test--)
-	solve();
+	int m, n;
+	cin >> m >> n;
+	vector<vector<int>> v(m, vector<int> (n, 0));
+	for (int i = 0; i < m; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cin >> v[i][j];
+		}
+	}
+
+	cout << solve(v, 2) << endl;
 }

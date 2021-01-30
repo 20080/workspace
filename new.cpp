535c7b7e9f006c750cf16339a64e5c6a25fa75a2
Suraj Thakur <surajkumar2091@gmail.com>
2021-01-30 16:50:34 +0530

    new.cpp maximal Sq Matrix LEETCD Mid

--- new.cpp
+++ new.cpp
@@ -1,53 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int functionf(int);
+int maximalSquare(vector<vector<char>>& matrix) {
 
-int dp(int);
-int lookup[10000] = {0};
+	int R = matrix.size(), C = matrix[0].size();
+	int res = 0;
+	vector<vector<int>>dp(R + 1, vector<int>(C + 1, 0));
+
+	for (int i = 1; i < R + 1; i++) { //remove -1 to and reduce dp mat by 1 and un comment the below code
+		for (int j = 1; j < C + 1; j++) {
+			// if(i==0||j==0){
+			//     dp[i][j]=matrix[i][j]-'0';
+			//     if(dp[i][j]&&res==0) res=1;
+			//     continue;
+			// }
+
+			if (matrix[i - 1][j - 1] != '0') {
+				int side = min(min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+				res = max(res, side);
+				cout << res << " ";
+				dp[i][j] = max(side, dp[i][j]);
+			}
+
+		}
+	}
+
+	return res * res;
+
+
+}
 
 int main() {										//507544127  193864606
 
 	//#ifdef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w," stdout);
+	freopen("output.txt", "w", stdout);
 	//#endif
-	int T;
-	int data;
-	cin >> T;
-	while (T > 0) {
-		cin >> data;
-		dp(data);
-		T--;
-	}
-
-}
-
-int dp(int data) {
-
-
-	lookup[0] = 0;
-	if (data == 0)
-		cout << lookup[0] << endl;
-
-	lookup[1] = 1;
-	if (data == 1)
-		cout << lookup[1] << endl;
-
-	if (data > 1) {
-		for (int i = 2; i <= data; i++) {
-			lookup[i] = lookup[i - 1] + lookup[i - 2];
-		}
-		int an = 0;
-		while (data >= 0) {
-			an += lookup[data] * lookup[data];
-			data--;
-		}
-
-		cout << an % 10000000000007 << endl;
-	}
-	return 0;
-
+//take input of matrix here
+	cout << (int)'0';//call funtion name to it;
 
 }
 
